431edb1dc8b326faf8b7d59b8c7f46049f5f86c8
Napster <Napster@DESKTOP-VS858NG>
2019-12-14 12:43:52 +0530

    initial commit

---
+++ ProjectHealth/src/test/java/com/docode/projecthealth/ProjectHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docode.projecthealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
